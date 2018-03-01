dcda53459e2b0eecb49c9f7c611824a6d2ecb23c
John Trudel <john_trudel@intuit.com>
2018-03-01 08:57:27 -0700

    jump start

---
+++ src/main/java/org/interview/Palindrome.java
@@ -0,0 +1,19 @@
+package org.interview;
+
+public class Palindrome {
+    public Palindrome() {
+        System.out.println("Constructor called");
+    }
+
+    public static boolean isPalindrome(String text) {
+        return false;
+    }
+
+    public static boolean isPalindrome(String text, boolean supportCase) {
+        return false;
+    }
+
+    public static boolean isPalindrome(String text, boolean supportCase, boolean supportPhrase) {
+        return false;
+    }
+}
---
+++ src/test/java/org/interview/PalindromeTest.java
@@ -0,0 +1,12 @@
+package org.interview;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class PalindromeTest {
+    @Test
+    public void palindrome() {
+
+    }
+}
